f25c4a47dcd680bc5e93add60efac7aa22a9c608
NikitaMuksinov <n.muxinoff@gmail.com>
2024-10-02 15:52:37 +0300

    Home_work_task3.py

--- lesson_1/home_task/task3.py
+++ lesson_1/home_task/task3.py
@@ -3,3 +3,7 @@
 # temperature = 25
 # Нужно обменять значения переменных местами. В итого age
 # должен равнятся 25 а temperature – 36.6:
+age = 36.6
+temperature = 25
+age,temperature = temperature,age
+print(age,temperature)
