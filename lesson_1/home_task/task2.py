018a0b5cc169a4549bb43151a94f59c4026794c0
NikitaMuksinov <n.muxinoff@gmail.com>
2024-10-02 16:44:22 +0300

    Home_work_test_task2.py

--- lesson_1/home_task/task2.py
+++ lesson_1/home_task/task2.py
@@ -1,17 +1,30 @@
 # todo: Преобразуйте переменную age и foo в число
 # age = "23"
 # foo = "23abc"
+age = "23"
+foo = "23abc"
+print(int(age[:]),(foo[:2]))
+age="23"
+foo="23abc"
+age=23
+foo=23
 #
 # Преобразуйте переменную age в Boolean
 # age = "123abc"
+age=True
+print(type(age))
 #
 # Преобразуйте переменную flag в Boolean
 # flag = 1
-#
+flag=1
+print(bool(flag))
 # Преобразуйте значение в Boolean
 # str_one = "Privet"
 # str_two = ""
-#
+str_one = True
+str_two = False
 # Преобразуйте значение 0 и 1 в Boolean
+print(bool(0),bool(1))
 #
 # Преобразуйте False в строку
+print(str(False))
