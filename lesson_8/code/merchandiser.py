c27a0d4daa95ac8bbe19620c408b528ee3f33128
vitaliy <chertkov.vitaliy@gmail.com>
2024-10-17 12:12:58 +0300

    Занятие 8

---
+++ lesson_8/code/merchandiser.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+# folder path
+DIR_PATH = '/home/vitaliy/download'
+os.chdir(DIR_PATH)
+
+# Функция возвращает множество расширений файлов
+def get_files_list():
+    # list to store files
+    res = []
+    # Iterate directory
+    for path in os.listdir(DIR_PATH):
+        # check if current path is a file
+        if os.path.isfile(os.path.join(DIR_PATH, path)):
+            res.append(path.split('.').pop(-1))
+    else:
+        return  set(res)
+
+
+def mk_dir_and_move(dirs):
+    for i in dirs:
+        if not os.path.exists(i):
+            os.mkdir(i)
+        os.system(f"mv *.{i}  {i}")
+
+
+def run():
+    dirs = get_files_list()
+    mk_dir_and_move(dirs)
+
+if __name__ == "__main__":
+    run()
