c29276b7f98549e768ffe385d2d23d5c21cc6f72
vitaliy <chertkov.vitaliy@gmail.com>
2024-11-21 14:22:05 +0300

    Урок 22

---
+++ lesson_22/code/game.py
@@ -0,0 +1,27 @@
+
+
+class Animal:
+    color = 'green'
+    def set (self, value):
+        class Animal_:
+            val = '100'
+        x = Animal_()
+        print(x.val)
+
+
+a = Animal()
+a.set('red')
+b = Animal()
+b.set('green')
+print(a.color)
+print(b.color)
+
+# print(Animal.x )
+
+# Animal.color = 'green'
+# b.color = 'green'
+#
+#
+
+#print(Animal.color)
+
